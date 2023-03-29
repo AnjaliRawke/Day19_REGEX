1bf6b05c55384ffcc37e150a299c78cb2370e5d7
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-29 20:09:59 +0530

    Added: UC3 User need to enter valid email

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -18,5 +18,13 @@ public class UserRegistration {
 			System.out.println("valid lastname");
 		else
 			System.out.println("Invalid lastname");
+
+		Pattern pattern2 = Pattern.compile("^( [a-z]{3} [.] [a-z]{3}  @ ([a-z]{2} [.] [a-z]{2}) [.][a-z]{2,})");
+		Matcher matcher2 = pattern.matcher("abc.xyz@bl.co.in");
+		if (matcher.matches())
+			System.out.println("valid email");
+		else
+			System.out.println("Invalid email");
+
 	}
 }
